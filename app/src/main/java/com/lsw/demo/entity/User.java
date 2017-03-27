6b39ee6f11dedc5b81389cdae75de9ac71d4510d
liushuwei <liushuwei@qiyi.com>
2017-03-27 11:09:35 +0800

    修改GreenDaoDemo的bug

--- app/src/main/java/com/lsw/demo/entity/User.java
+++ app/src/main/java/com/lsw/demo/entity/User.java
@@ -12,27 +12,33 @@ import org.greenrobot.greendao.annotation.Generated;
 @Entity
 public class User {
     @Id
-    private Long id;
+    private Long id;//id的类型写为Long,不是小写的long，否则 id 不能自增长。
     private String name;
-    @Transient
+    @Transient//添加此标记之后不会生成数据库表的列
     private int tempUsageCount; // not persisted
+
     @Generated(hash = 873297011)
     public User(Long id, String name) {
         this.id = id;
         this.name = name;
     }
+
     @Generated(hash = 586692638)
     public User() {
     }
+
     public Long getId() {
         return this.id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return this.name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
